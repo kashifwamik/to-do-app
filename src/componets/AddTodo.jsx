37fa1f9c4980ee82fa785f6323eacb0404007f1e
kashifwamik <wamikkashif160@gamil.com>
2024-08-23 01:09:58 +0530

    changed readme and Used useRef in addTodo component

--- src/componets/AddTodo.jsx
+++ src/componets/AddTodo.jsx
@@ -1,36 +1,30 @@
 import "./AddTodo.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { BiMessageAdd } from "react-icons/bi";
 function AddTodo({ onNewItem }) {
-  const [itemName, setItemName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const handleNameChange = (e) => {
-    setItemName(e.target.value);
-  };
-  const handleDateChange = (e) => {
-    setDueDate(e.target.value);
-  };
-  const handleAddClick = (e) => {
-    console.log(e);
-    e.preventDefault();
+  const itemNameElement = useRef();
+  const dueDateElement = useRef();
 
+  const handleAddClick = (e) => {
+    e.preventDefault();
+    const itemName = itemNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    itemNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(itemName, dueDate);
-    setItemName("");
-    setDueDate("");
   };
   return (
     <div className="container">
       <form className="row ks-row" onSubmit={handleAddClick}>
         <div className="col-5 ks-text">
           <input
+            ref={itemNameElement}
             type="text"
-            value={itemName}
             placeholder="Enter Todo Here"
-            onChange={handleNameChange}
           />
         </div>
         <div className="col-3 ks-date">
-          <input type="date" onChange={handleDateChange} value={dueDate} />
+          <input ref={dueDateElement} type="date" />
         </div>
         <div className="col-2">
           <button className="btn btn-success ks-button">
